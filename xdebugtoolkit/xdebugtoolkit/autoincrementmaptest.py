67dd9cb7fe896520da10cd5fd531e5d96f956672
alexey.kupershtokh <alexey.kupershtokh@cb3d38b2-a721-11dd-81d7-27b247425303>
2009-05-09 11:12:59 +0000

    * autoincrementmap package with an AutoIncrementMap class, which is future replacement for the AutoFilling class  * autoincrementmaptest package with tests for the AutoIncrementMap class

---
+++ xdebugtoolkit/xdebugtoolkit/autoincrementmaptest.py
@@ -0,0 +1,86 @@
+'''
+Created on May 9, 2009
+
+@author: wicked
+'''
+import unittest
+from autoincrementmap import AutoIncrementMap
+
+class Test(unittest.TestCase):
+
+
+    def testZeroLen(self):
+        map = AutoIncrementMap()
+        self.assertEquals(len(map), 0)
+        
+    def testIncrementingLen(self):    
+        map = AutoIncrementMap()
+        map['a']
+        self.assertEquals(len(map), 1)
+        map['b']
+        self.assertEquals(len(map), 2)
+        
+    def testNonIncrementingLen(self):    
+        map = AutoIncrementMap()
+        map['a']
+        map['b']
+        self.assertEquals(len(map), 2)
+        map['a']
+        map['b']
+        self.assertEquals(len(map), 2)
+        
+    def testContains(self):
+        map = AutoIncrementMap()
+        self.assertFalse('a' in map)
+        self.assertFalse('b' in map)
+        map['a']
+        self.assertTrue('a' in map)
+        self.assertFalse('b' in map)
+
+    def testHasKey(self):
+        map = AutoIncrementMap()
+        self.assertFalse(map.has_key('a'))
+        self.assertFalse(map.has_key('b'))
+        map['a']
+        self.assertTrue(map.has_key('a'))
+        self.assertFalse(map.has_key('b'))
+        
+    def testGet(self):
+        map = AutoIncrementMap()
+        map['a']
+        map['b']
+        self.assertEquals(map['a'], 0)
+        self.assertEquals(map['b'], 1)
+        
+    def testGetById(self):
+        map = AutoIncrementMap()
+        map['a']
+        map['b']
+        self.assertEquals(map.get_by_id(0), 'a')
+        self.assertEquals(map.get_by_id(1), 'b')
+        
+    def testMerge(self):
+        map1 = AutoIncrementMap()
+        map1['a1']
+        map1['b1']
+        map2 = AutoIncrementMap()
+        map1['b1']
+        map2['a2']
+        map2['b2']
+        map1.merge(map2)
+        
+        self.assertEquals(len(map1), 4)
+        
+        self.assertTrue('a1' in map1)
+        self.assertTrue('b1' in map1)
+        self.assertTrue('a2' in map1)
+        self.assertTrue('b2' in map1)
+
+        self.assertEquals(map1['b2'], 3)
+        self.assertEquals(map1['a2'], 2)
+        self.assertEquals(map1['b1'], 1)
+        self.assertEquals(map1['a1'], 0)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testLen']
+    unittest.main()
