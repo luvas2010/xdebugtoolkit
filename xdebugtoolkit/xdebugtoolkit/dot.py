0153c2ed9a72dd48de9e5f33148ae508af58e887
alexey.kupershtokh <alexey.kupershtokh@cb3d38b2-a721-11dd-81d7-27b247425303>
2009-05-10 17:19:50 +0000

    + use ofoptparse for multiple files and dynamic threshold  + merging of trees  + RawTree to .cg convertation  * got rid of fl_map and fn_map in favor of flyweight pattern for storing file and function names  * added some unit tests  * added some documentation  * many minor fixes and refactorings

--- xdebugtoolkit/xdebugtoolkit/dot.py
+++ xdebugtoolkit/xdebugtoolkit/dot.py
@@ -4,20 +4,6 @@ class DotBuilder:
         node_styler = node_styler_class(tree.get_max_self_time(),
             tree.get_total_time(), tree.get_max_call_count(),
             tree.get_total_call_count())
-        fn_rev = tree.fn_map.rev()
-        for i in fn_rev:
-            if fn_rev[i].startswith('php::'):
-                fn_rev[i] = fn_rev[i][5:]
-            if fn_rev[i].startswith('require::'):
-                fn_rev[i] = fn_rev[i][9:]
-            if fn_rev[i].startswith('require_once::'):
-                fn_rev[i] = fn_rev[i][14:]
-            if fn_rev[i].startswith('include::'):
-                fn_rev[i] = fn_rev[i][9:]
-            if fn_rev[i].startswith('include_once::'):
-                fn_rev[i] = fn_rev[i][14:]
-            if len(fn_rev[i]) > 30:
-                fn_rev[i] = fn_rev[i][0:12] + '...' + fn_rev[i][-15:]
         graph = []
         
         graph.append('digraph G { \n')
@@ -40,11 +26,17 @@ class DotBuilder:
             parent_id = '/'.join(map(str, stack_pos[0:-2]));
             self_id = '/'.join(map(str, stack_pos[0:-1]));
             color = "#%02x%02x%02x" % node_styler.colorize(stack[-1])
+
+            # trim fn a bit
+            fn = str(stack[-1].fn.get_clean())
+            if len(fn) > 30:
+                fn = fn[0:12] + '...' + fn[-15:]
+                
             if stack[-1].call_count == 1:
-                graph.append('"%s" [label="%s\\n%sms" color="%s"]; \n' % (self_id, fn_rev[stack[-1].fn], stack[-1].sum_self_time/1000, color))
+                graph.append('"%s" [label="%s\\n%sms" color="%s"]; \n' % (self_id, fn, stack[-1].sum_self_time/1000, color))
                 graph.append('"%s" -> "%s" [label="%sms"]; \n' % (parent_id, self_id, stack[-1].sum_inclusive_time/1000))
             else:
-                graph.append('"%s" [label="%s\\n%sx\[%sms..%sms] = %sms" color="%s"]; \n' % (self_id, fn_rev[stack[-1].fn], stack[-1].call_count, stack[-1].min_self_time/1000, stack[-1].max_self_time/1000, stack[-1].sum_self_time/1000, color))
+                graph.append('"%s" [label="%s\\n%sx\[%sms..%sms] = %sms" color="%s"]; \n' % (self_id, fn, stack[-1].call_count, stack[-1].min_self_time/1000, stack[-1].max_self_time/1000, stack[-1].sum_self_time/1000, color))
                 graph.append('"%s" -> "%s" [label="%sx\[%sms..%sms] = %sms"]; \n' % (parent_id, self_id, stack[-1].call_count, stack[-1].min_inclusive_time/1000, stack[-1].max_inclusive_time/1000, stack[-1].sum_inclusive_time/1000))
 
             # cleanup stack
