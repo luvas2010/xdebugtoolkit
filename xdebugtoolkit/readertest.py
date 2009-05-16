b3b47baecd661cf855ddaf90119ee863dc50ec32
alexey.kupershtokh <alexey.kupershtokh@cb3d38b2-a721-11dd-81d7-27b247425303>
2009-05-16 13:03:45 +0000

    * Added initial tests for the reader package;  * refactored CallTree.__str__ and made it more verbose;

---
+++ xdebugtoolkit/readertest.py
@@ -0,0 +1,31 @@
+'''
+Created on May 16, 2009
+
+@author: wicked
+'''
+import unittest
+from cgparser import *
+from reader import *
+
+
+class Test(unittest.TestCase):
+    
+    #filename = 'fixtures/cachegrind.out.2776'
+    filename = 'fixtures/1241577166_325172.cg'
+    
+    def setUp(self):
+        self.parser = XdebugCachegrindFsaParser(self.filename)
+        self.tree_builder = XdebugCachegrindTreeBuilder(self.parser)
+
+    def testGetTree(self):
+        tree = self.tree_builder.get_tree()
+        print tree
+        self.assertEquals(tree.get_max_self_time(), 23525)
+        self.fail('tree.get_total_time() test is not implemented yet')
+        self.assertEquals(tree.get_max_call_count(), 1)
+        self.assertEquals(tree.get_total_call_count(), 14)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
