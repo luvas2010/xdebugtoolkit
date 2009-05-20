1e7d57774232aa17c51744000f25c444fab62876
alexey.kupershtokh <alexey.kupershtokh@cb3d38b2-a721-11dd-81d7-27b247425303>
2009-05-20 04:38:50 +0000

    * Added a test: Merge to empty tree should preserve global tree statistics

--- xdebugtoolkit/readertest.py
+++ xdebugtoolkit/readertest.py
@@ -24,6 +24,19 @@ class Test(unittest.TestCase):
         self.assertEquals(tree.get_max_call_count(), 1)
         self.assertEquals(tree.get_total_call_count(), 14)
 
+    def testMergeToEmpty(self):
+        empty_tree = CallTree()
+        tree = self.tree_builder.get_tree()
+        self.assertEquals(empty_tree.get_max_self_time(), 0)
+        self.assertEquals(empty_tree.get_total_time(), 0)
+        self.assertEquals(empty_tree.get_max_call_count(), 0)
+        self.assertEquals(empty_tree.get_total_call_count(), 0)
+        empty_tree.merge(tree)
+        self.assertEquals(empty_tree.get_max_self_time(), 23525)
+        self.assertEquals(empty_tree.get_total_time(), 23798)
+        self.assertEquals(empty_tree.get_max_call_count(), 1)
+        self.assertEquals(empty_tree.get_total_call_count(), 14)
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
